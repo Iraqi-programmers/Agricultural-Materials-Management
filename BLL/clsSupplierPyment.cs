4a09398f3269b20743f32cdffad8662844ee7ea1
mohammed Abbas <108941553+mohammedcoder23@users.noreply.github.com>
2025-02-15 19:28:41 +0300

    Adding Supplier Pyment Data class  for DLL  and  Supplier Pyment Class for DAL

---
+++ BLL/clsSupplierPyment.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class clsSupplierPyment
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode _Mode = enMode.AddNew;
+
+        public int SupplierPymentID { get; set; }
+        public decimal Amount { get; set; }
+        public int SupplierID { get; set; }
+        public DateTime PymentDate { get; set; }
+        public int PurchaseID { get; set; }
+        public int UserID { get; set; }
+
+        private clsSupplierPyment(int supplierPymentID, decimal amount, int supplierID, DateTime pymentDate, int purchaseID, int userID)
+        {
+            SupplierPymentID = supplierPymentID;
+            Amount = amount;
+            SupplierID = supplierID;
+            PymentDate = pymentDate;
+            PurchaseID = purchaseID;
+            UserID = userID;
+            _Mode = enMode.Update;
+        }
+
+        public clsSupplierPyment()
+        {
+            SupplierPymentID = -1;
+            Amount =0;
+            SupplierID =-1;
+            PymentDate =DateTime.Now;
+            PurchaseID = -1;
+            UserID = -1;
+            _Mode = enMode.AddNew;  
+        }
+
+        private async Task<bool> _AddNewSupplierPyment()
+        {
+            this.SupplierPymentID = (int)await clsSupplierPymentsData.AddSupplierPyment(this.Amount, this.SupplierID, this.PymentDate,this.PurchaseID,this.UserID);
+            return this.SupplierPymentID != -1;
+        }
+
+        private async Task<bool> _UpdateSupplierPyments()
+        {
+            return await clsSupplierPymentsData.UpdateSupplierPyment(this.SupplierPymentID,this.Amount,this.PymentDate,this.PurchaseID,this.UserID) ;
+        }
+
+        public async Task<bool> Save()
+        {
+            switch (_Mode)
+            {
+                case enMode.AddNew:
+                    {
+                        if (await _AddNewSupplierPyment())
+                        {
+                            _Mode = enMode.Update;
+                            return true;
+                        }
+                        else
+                        { return false; }
+
+                    }
+                case enMode.Update:
+                    {
+                        return await _UpdateSupplierPyments();
+                    }
+
+            }
+            return false;
+
+        }
+
+        public static async Task<DataTable?> GetAllSupplierPyments()
+        {
+            return await clsSupplierPymentsData.GetAllSupplierPayments();
+        }
+
+        public static async Task<bool> DeleteSupplierPymentByID(int id)
+        {
+            return await clsSupplierPymentsData.DeleteSupplierPyment(id);
+        }
+
+        public async Task<object[]?> Find(int id)
+        {
+            return await clsSupplierPymentsData.GetSupplierPymentBySupplierID(id);
+        }
+    }
+}
---
+++ DAL/clsSupplierPymentsData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using MyLib_DotNet.DatabaseExecutor;
+
+namespace DAL
+{
+    public class clsSupplierPymentsData
+    {
+        public static async Task<int?> AddSupplierPyment( decimal Amount,int SupplierID,DateTime PymentDate,int PurchaseID,int UserID)
+        {
+            SqlParameter[] Parameters =
+            {
+                new SqlParameter("@Amount", Amount),
+                new SqlParameter("@SupplierID", SupplierID),
+                new SqlParameter("@PaymentDate", PymentDate),
+                new SqlParameter("@PurchaseID",PurchaseID),
+                new SqlParameter("@UserID", UserID)
+            };
+
+            return await CRUD.AddAsync("SP_AddSupplierPayment", Parameters, CommandType.StoredProcedure);
+        }
+
+        public static async Task<bool> UpdateSupplierPyment(int SupplierPaymentID, decimal Amount, DateTime PymentDate, int PurchaseID, int UserID)
+        {
+            SqlParameter[] Parameters =
+            {
+                new SqlParameter("@SupplierPaymentID", SupplierPaymentID),
+                new SqlParameter("@Amount", Amount),
+                new SqlParameter("@PaymentDate", PymentDate),
+                new SqlParameter("@PurchaseID",PurchaseID),
+                new SqlParameter("@UserID", UserID)
+            };
+
+            return await CRUD.UpdateAsync("SP_UpdateSupplierPayment", Parameters, CommandType.StoredProcedure);
+
+        }
+
+        public static async Task<DataTable?> GetAllSupplierPayments()
+        {
+            return await CRUD.GetAllAsDataTableAsync("SP_GetAllSupplierPayments");
+        }
+
+        public static async Task<bool> DeleteSupplierPyment(int ID)
+        {
+            return await CRUD.DeleteAsync("SP_DeleteSupplierPayment","@SupplierPaymentID",ID, CommandType.StoredProcedure);
+        }
+
+        public static async Task<object[]?> GetSupplierPymentBySupplierID(int SupplierID)
+        {
+            return await CRUD.GetByColumnValueAsync("SP_GetSupplierPaymentsBySupplier","@SupplierID", SupplierID, CommandType.StoredProcedure);
+        }
+
+    }
+}
