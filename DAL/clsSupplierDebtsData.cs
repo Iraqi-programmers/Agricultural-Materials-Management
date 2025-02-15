348bd92aabf11de4a04e87458971fd99be8eb0ac
mohammed Abbas <108941553+mohammedcoder23@users.noreply.github.com>
2025-02-15 13:46:06 +0300

    Adding SupplierDebts data acsses layer class and BLL class

---
+++ DAL/clsSupplierDebtsData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using MyLib_DotNet.DatabaseExecutor;
+
+namespace DAL
+{
+    public class clsSupplierDebtsData
+    {
+        public static async Task<int?> AddSupplierDebts(int SupplierID, decimal Amount, DateTime DeptPaymentDate)
+        {
+            SqlParameter[] Parameters =
+            {
+                new SqlParameter("@SupplierID", SupplierID),
+                new SqlParameter("@Amount",Amount),
+                new SqlParameter("@DebtPaymentDate",DeptPaymentDate)
+            };
+
+            return await CRUD.AddAsync("SP_AddSupplierDebt", Parameters, CommandType.StoredProcedure);
+        }
+
+
+
+        public static async Task<bool> UpdateSupplierDebt(int SupplierDebtID, decimal Amount, DateTime DeptPaymentDate)
+        {
+            SqlParameter[] Parameters =
+            {
+                new SqlParameter("@SupplierDebtID", SupplierDebtID),
+                new SqlParameter("@Amount",Amount),
+                new SqlParameter("@DebtPaymentDate",DeptPaymentDate)
+            };
+
+            return await CRUD.UpdateAsync("SP_UpdateSupplierDebt", Parameters, CommandType.StoredProcedure);
+
+        }
+
+        public static async Task<DataTable?> GetAllSupplierDept()
+        {
+            return await CRUD.GetAllAsDataTableAsync("SP_GetAllSuppliersDebts");
+        }
+
+        public static async Task<bool> DeleteSupplierDebt(int ID)
+        {
+            return await CRUD.DeleteAsync("SP_DeleteSupplierDebt", "@SupplierDebtID", ID, CommandType.StoredProcedure);
+        }
+
+        public static async Task<object[]?> GetSupplierDebtByID(int ID)
+        {
+            return await CRUD.GetByColumnValueAsync("SP_GetSupplierDebtsByID", "@SupplierID", ID, CommandType.StoredProcedure);
+        }
+
+
+
+
+    }
+
+
+
+
+
+} 
+
+
+    
+ 
