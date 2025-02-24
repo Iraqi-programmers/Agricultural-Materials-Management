59904fd5fe471f5d73e6d5798b3e82413955b9b7
Mortaza <thegit111@gmail.com>
2025-02-24 17:24:46 +0300

    Updated clsPersonData, clsPerson

--- BLL/clsPerson.cs
+++ BLL/clsPerson.cs
@@ -27,6 +27,12 @@ namespace BLL
             Address = address;
         }
 
+        public async Task<bool> AddAsync()
+        {
+            Id = await clsPersonData.AddAsync(FullName, NationalNum, PhoneNumber, Address);
+            return Id != null;
+        }
+
         public static async Task<clsPerson?> GetPersonByIdAsync(int personId)
         {
             var data = await clsPersonData.GetPersonInfoByIdAsync(personId);
@@ -46,40 +52,25 @@ namespace BLL
             return FetchPersonData(ref data);
         }
 
-        public static async Task<Dictionary<string, object>?> GetPersonFullDataByIdAsync(int personId)
-            => await clsPersonData.GetPersonFullDataByIdAsync(personId);
-        public static async Task<Dictionary<string, object>?> GetPersonFullDataByFullNameAsync(string fullName)
-            => await clsPersonData.GetPersonFullDataByFullNameAsync(fullName);
-        public static async Task<Dictionary<string, object>?> GetPersonFullDataPhoneNumAsync(string phoneNum)
-            => await clsPersonData.GetPersonFullDataPhoneNumAsync(phoneNum);
+        public static async Task<Dictionary<string, object>?> GetPersonFullDataByIdAsync(int personId) => await clsPersonData.GetPersonFullDataByIdAsync(personId);
+        public static async Task<Dictionary<string, object>?> GetPersonFullDataByFullNameAsync(string fullName) => await clsPersonData.GetPersonFullDataByFullNameAsync(fullName);
+        public static async Task<Dictionary<string, object>?> GetPersonFullDataPhoneNumAsync(string phoneNum) => await clsPersonData.GetPersonFullDataPhoneNumAsync(phoneNum);
 
-        public static async Task<DataTable?> GetAllPeopleAsDataTableAsync() => await clsPersonData.GetAllPersonsAsDataTableAsync();
-        //public static async Task<List<Dictionary<string, object>>?> GetAllPeopleAsListAsync() => await clsPersonData.GetAllPersonsAsListAsync();
+        public static async Task<DataTable?> GetAllAsDataTableAsync() => await clsPersonData.GetAllAsDataTableAsync();
+        public static async Task<List<Dictionary<string, object>>?> GetAllAsListAsync() => await clsPersonData.GetAllAsListAsync();
 
-        //public static async Task<bool> IsPersonExistByIdAsync(int personId) => await clsPersonData.IsPersonExistAsync(personId);
-        //public static async Task<bool> IsPersonExistByFullNameAsync(string fullName) => await clsPersonData.IsPersonExistByFullNameAsync(fullName);
-        //public static async Task<bool> IsPersonExistByNationalNumAsync(string nationalNum) => await clsPersonData.IsPersonExistByNationalNumAsync(nationalNum);
+        public static async Task<bool> IsPersonExistByIdAsync(int personId) => await clsPersonData.IsPersonExistAsync(personId);
+        public static async Task<bool> IsPersonExistByFullNameAsync(string fullName) => await clsPersonData.IsPersonExistByFullNameAsync(fullName);
+        public static async Task<bool> IsPersonExistByNationalNumAsync(string nationalNum) => await clsPersonData.IsPersonExistByNationalNumAsync(nationalNum);
 
-        public async Task<bool> AddAsync()
-        {
-            Id = await clsPersonData.AddNewPersonAsync(FullName, NationalNum, PhoneNumber, Address);
-            return Id != null;
-        }
+        public async Task<bool> UpdateAsync() => await clsPersonData.UpdateAsync(Id, FullName, NationalNum, PhoneNumber, Address);
 
-        public async Task<bool> UpdatePersonDataAsync() => await clsPersonData.UpdatePersonDataAsync(Id, FullName, NationalNum, PhoneNumber, Address);
-
-        public static async Task<bool> DeleteAsync(int? personId) => await clsPersonData.DeletePersonByIdAsync(personId);
+        public static async Task<bool> DeleteByIdAsync(int? personId) => await clsPersonData.DeletePersonByIdAsync(personId);
         public static async Task<bool> DeleteByFullNameAsync(string fullName) => await clsPersonData.DeletePersonByFullNameAsync(fullName);
         public static async Task<bool> DeleteByNationalNumAsync(string nationalNum) => await clsPersonData.DeletePersonByNationalNumAsync(nationalNum);
 
-        public async Task<bool> DeleteAsync() => await DeleteAsync(Id);
-        public async Task<bool> DeleteByFullNameAsync() => await DeleteByFullNameAsync(FullName);
-        public async Task<bool> DeleteByNationalNumAsync()
-        {
-            if (NationalNum == null) return false;
-            return await DeleteByNationalNumAsync(NationalNum);
-        }
-
+        public async Task<bool> DeleteAsync() => await DeleteByIdAsync(Id);
+        
         public static clsPerson FetchPersonData(ref Dictionary<string, object> dict)
         {
             return new clsPerson(
--- DAL/clsPersonData.cs
+++ DAL/clsPersonData.cs
@@ -6,84 +6,7 @@ namespace DAL
 {
     public class clsPersonData
     {
-        public static async Task<Dictionary<string, object>?> GetPersonInfoByIdAsync(int personId)
-            => await CRUD.GetByColumnValueAsync("SP_", "PersonID", personId);
-        public static async Task<Dictionary<string, object>?> GetPersonInfoByFullNameAsync(string fullName)
-            => await CRUD.GetByColumnValueAsync("SP_", "FullName", fullName);
-        public static async Task<Dictionary<string, object>?> GetPersonInfoByNationalNumAsync(string nationalNum)
-            => await CRUD.GetByColumnValueAsync("SP_", "NationalNum", nationalNum);
-
-        /*
-        CREATE OR ALTER VIEW vw_PersonDetails AS
-        SELECT 
-            p.PersonID,
-            p.FullName,
-            p.NationalNum,
-            p.PhoneNumber,
-            p.Address,
-
-            -- بيانات المدفوعات
-            py.Amount AS PaymentAmount,
-            py.Date AS PaymentDate,
-            py.UserID AS PaymentUserID,
-            u1.UserName AS PaymentUserName,
-
-            -- بيانات الديون
-            d.DebtAmount,
-            d.DebtPaymentDate,
-
-            -- بيانات المبيعات
-            s.SaleID,
-            s.SaleDate,
-            s.UserID AS SaleUserID,
-            u2.UserName AS SaleUserName,
-
-            -- تفاصيل المبيعات
-            sd.StockID,
-            sd.Quantity,
-            sd.WarrintyDate,
-            sd.Price, 
-            sd.TotalCost
-
-        FROM Person p
-        LEFT JOIN Payments py ON p.PersonID = py.PersonID
-        LEFT JOIN Users u1 ON py.UserID = u1.UserID
-        LEFT JOIN Debts d ON p.PersonID = d.PersonID
-        LEFT JOIN Sales s ON p.PersonID = s.PersonID
-        LEFT JOIN Users u2 ON s.UserID = u2.UserID
-        LEFT JOIN SaleDetails sd ON s.SaleID = sd.SaleID;
-
-         */
-        /*
-        CREATE PROCEDURE SP_GetPersonFullDetailsByID
-            @PersonID INT
-        AS
-        BEGIN
-            SELECT * FROM vw_PersonDetails WHERE PersonID = @PersonID;
-        END;
-         */
-        public static async Task<Dictionary<string, object>?> GetPersonFullDataByIdAsync(int personId)
-            => await CRUD.GetByColumnValueAsync("SP_", "PersonID", personId);
-        public static async Task<Dictionary<string, object>?> GetPersonFullDataByFullNameAsync(string fullName)
-            => await CRUD.GetByColumnValueAsync("SP_", "FullName", fullName);
-        public static async Task<Dictionary<string, object>?> GetPersonFullDataPhoneNumAsync(string phoneNum)
-            => await CRUD.GetByColumnValueAsync("SP_", "PhoneNumber", phoneNum);
-
-        public static async Task<DataTable?> GetAllPersonsAsDataTableAsync()
-            => await CRUD.GetAllAsDataTableAsync("SP_");
-        //public static async Task<List<Dictionary<string, object>>?> GetAllPersonsAsListAsync() => await CRUD.GetAllAsListAsync("SP_");
-
-        public static async Task<bool> IsPersonExistAsync(int? personId)
-        {
-            if (personId == null) return false;
-            return await CRUD.IsExistAsync("SP_", "PersonID", personId);
-        }
-        public static async Task<bool> IsPersonExistByFullNameAsync(string fullName)
-            => await CRUD.IsExistAsync("SP_", "FullName", fullName);
-        public static async Task<bool> IsPersonExistByNationalNumAsync(string nationalNum)
-            => await CRUD.IsExistAsync("SP_", "NationalNum", nationalNum);
-
-        public static async Task<int?> AddNewPersonAsync(string fullName, string? nationalNum, string? phoneNum, string? address)
+        public static async Task<int?> AddAsync(string fullName, string? nationalNum, string? phoneNum, string? address)
         {
             SqlParameter[] parameters =
             {
@@ -95,7 +18,31 @@ namespace DAL
             return await CRUD.AddAsync("SP_", parameters);
         }
 
-        public static async Task<bool> UpdatePersonDataAsync(int? personId, string fullName, string? nationalNum, string? phoneNum, string? address)
+        public static async Task<Dictionary<string, object>?> GetPersonInfoByIdAsync(int personId)
+            => await CRUD.GetByColumnValueAsync("SP_", "PersonID", personId);
+        public static async Task<Dictionary<string, object>?> GetPersonInfoByFullNameAsync(string fullName)
+            => await CRUD.GetByColumnValueAsync("SP_", "FullName", fullName);
+        public static async Task<Dictionary<string, object>?> GetPersonInfoByNationalNumAsync(string nationalNum)
+            => await CRUD.GetByColumnValueAsync("SP_", "NationalNum", nationalNum);
+
+        public static async Task<Dictionary<string, object>?> GetPersonFullDataByIdAsync(int personId)
+            => await CRUD.GetByColumnValueAsync("SP_", "PersonID", personId);
+        public static async Task<Dictionary<string, object>?> GetPersonFullDataByFullNameAsync(string fullName)
+            => await CRUD.GetByColumnValueAsync("SP_", "FullName", fullName);
+        public static async Task<Dictionary<string, object>?> GetPersonFullDataPhoneNumAsync(string phoneNum)
+            => await CRUD.GetByColumnValueAsync("SP_", "PhoneNumber", phoneNum);
+
+        public static async Task<DataTable?> GetAllAsDataTableAsync() => await CRUD.GetAllAsDataTableAsync("SP_");
+        public static async Task<List<Dictionary<string, object>>?> GetAllAsListAsync() => await CRUD.GetAllAsListAsync("SP_");
+
+        public static async Task<bool> IsPersonExistAsync(int personId)
+            => await CRUD.IsExistAsync("SP_", "PersonID", personId);
+        public static async Task<bool> IsPersonExistByFullNameAsync(string fullName)
+            => await CRUD.IsExistAsync("SP_", "FullName", fullName);
+        public static async Task<bool> IsPersonExistByNationalNumAsync(string nationalNum)
+            => await CRUD.IsExistAsync("SP_", "NationalNum", nationalNum);
+
+        public static async Task<bool> UpdateAsync(int? personId, string fullName, string? nationalNum, string? phoneNum, string? address)
         {
             SqlParameter[] parameters =
             {
