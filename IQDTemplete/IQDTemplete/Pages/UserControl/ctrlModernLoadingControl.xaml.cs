c2229eb68a8c44860c60ae5130019fe2766b92ea
Abo_Sanad <167923142+AbuTharHatem95@users.noreply.github.com>
2025-03-09 02:22:47 +0300

    Add Tow  UserControl (ctrlModernLoding , ctrlPurchesesListDetiles) Update ViewPurchesesList , ctrlSalesListDetils

---
+++ IQDTemplete/IQDTemplete/Pages/UserControl/ctrlModernLoadingControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Interface.Pages.UserControl
+{
+   
+    public partial class ctrlModernLoadingControl : System.Windows.Controls.UserControl
+    {
+        public ctrlModernLoadingControl()
+        {
+            InitializeComponent();
+            this.Loaded += OnLoaded; 
+        }
+
+        private void StartAnimation()
+        {
+            var storyboard = (Storyboard)Resources["RotateAnimation"];
+            storyboard.Begin();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            StartAnimation(); 
+        }
+    }
+}
+
+
+
--- IQDTemplete/IQDTemplete/Pages/UserControl/ctrlSalesListDetails.xaml.cs
+++ IQDTemplete/IQDTemplete/Pages/UserControl/ctrlSalesListDetails.xaml.cs
@@ -1,46 +1,24 @@
-﻿namespace Interface.Pages.UserControl
+﻿using System.Windows;
+
+namespace Interface.Pages.UserControl
 {
-    /// <summary>
-    /// Interaction logic for ctrlSalesListDetails.xaml
-    /// </summary>
+    
     public partial class ctrlSalesListDetails : System.Windows.Controls.UserControl
     {
-        // نموذج بيانات تفاصيل القائمة
-        public class InvoiceDetails
-        {
-            public string InvoiceNumber { get; set; }
-            public string StoreNumber { get; set; }
-            public string WarrantyDate { get; set; }
-            public decimal Price { get; set; }
-            public int Quantity { get; set; }
-            public decimal TotalAmount { get; set; }
-            public string InvoiceDate { get; set; }
-            public string CustomerName { get; set; }
-            public string NationalID { get; set; }
-            public string UserName { get; set; }
-        }
         public ctrlSalesListDetails()
         {
             InitializeComponent();
-            DataContext = this;
 
         }
 
-        // خاصية لعرض تفاصيل القائمة
-        public InvoiceDetails InvoiceDetail { get; set; }
-
-        // خاصية لعرض القوائم الفرعية
-        public System.Collections.ObjectModel.ObservableCollection<SubInvoice> SubInvoices { get; set; }
-
-
-       
-
-        // نموذج بيانات القوائم الفرعية
-        public class SubInvoice
+        private void CustomerName_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            public string InvoiceNumber { get; set; }
-            public string Date { get; set; }
-            public string UserName { get; set; }
+            MessageBox.Show("جاسم");
+        }
+
+        private void UserInfo_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
         }
     }
 }
